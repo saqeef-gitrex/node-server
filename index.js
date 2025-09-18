fc565cde109c7075106fcbbe3f14636786b246a1
Muhammad Saqeef <saqeef.gitrex@gmail.com>
2025-09-18 11:17:58 +0500

    Fixed API Endpoint

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ import dotenv from "dotenv"
 const app = express()
 
 dotenv.config()
-
+app.use(express.json())
 
 app.get("/",async(req,res)=>{
-    const data = await fetch("https://dummyjson.com/users")
+    
+    const {limit,skip}=req.query
+    console.log(limit,skip)
+    const data = await fetch(`https://dummyjson.com/users?limit=${limit}&skip=${skip}`)
     .then((res)=>res.json())
     .catch((err)=>console.log("Error fetching data"))
     .finally(()=>console.log("Promise executed"))
+
     
     return res.json({
         success : true,
         data
     }).status(200)
 })
-
-export default app
+// app.listen(process.env.PORT)
+ export default app
